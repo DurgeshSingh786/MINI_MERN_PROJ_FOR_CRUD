e9d1d9fec79a854d4e22a42d4c1490f1feeff38e
DurgeshSingh786 <durgeshsingh21122099@gmail.com>
2022-04-25 20:21:38 +0530

    Create user module added

--- backend/server/controller/controller.js
+++ backend/server/controller/controller.js
@@ -1,3 +1,36 @@
-var Userdb=require('../model/model');
+const { rawListeners } = require("../model/model");
+var Userdb = require("../model/model");
 
-//
+//create new user and push into DB
+exports.create = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: "content cannot be empty" });
+    return res;
+  }
+
+  var user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while creating a create operation",
+      });
+    });
+};
+
+exports.find = (req, res) => {};
+
+exports.update = (req, res) => {};
+
+exports.delete = (req, res) => {};
--- backend/server/routes/router.js
+++ backend/server/routes/router.js
@@ -3,10 +3,26 @@ const route = require("express").Router();
 
 const services = require("../services/render");
 
+//here we are including controller module to use api
+var controller = require("../controller/controller");
+
 route.get("/", services.homeroutes);
 
 route.get("/add-user", services.add_user);
 
 route.get("/update-user", services.update_user);
+
+//===================================API========================================================================
+//when we get POST req for PATH:api/users, then we just need to call create() from controller.js
+route.post("/api/users", controller.create);
+
+//when we get GET req for PATH:api/users, then we just need to call FIND() from controller.js
+route.get("/api/users", controller.find);
+
+//when we get PUT req for PATH:api/users/:id, then we just need to call update() from controller.js
+route.put("/api/users/:id", controller.update);
+
+//when we get DELETE req for PATH:api/users/:id, then we just need to call delete() from controller.js
+route.delete("/api/users/:id", controller.delete);
 
 module.exports = route;
